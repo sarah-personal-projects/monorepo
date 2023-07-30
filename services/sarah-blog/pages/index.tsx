a5ee9b91d756947e23605fcacbb6c445622edf7e
YooJinRa <rarastudy@naver.com>
2023-07-30 15:23:02 +0900

    feat: 공통 컴포넌트 의존성 삭제

--- services/sarah-blog/pages/index.tsx
+++ services/sarah-blog/pages/index.tsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { sayHello } from "@common/lib";
 
 const MainPage = () => {
-	return (
-		<div>
-			MAIN_PAGE <i>{sayHello()}</i>
-		</div>
-	);
+	return <div>MAIN_PAGE</div>;
 };
 
 export default MainPage;
