ed1634e343277711bf7d2f76cfef244e070bd6b0
YooJinRa <rarastudy@naver.com>
2023-07-30 15:02:05 +0900

    feat: 공통컴포넌트 삽입 테스트 진행

--- services/sarah-blog/pages/index.tsx
+++ services/sarah-blog/pages/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { sayHello } from "@common/lib";
 
 const MainPage = () => {
-	return <div>main page</div>;
+	return (
+		<div>
+			MAIN_PAGE <i>{sayHello()}</i>
+		</div>
+	);
 };
 
 export default MainPage;
